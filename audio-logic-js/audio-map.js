678d83d930e0edf7f5322d1265eab16fffd4db2c
ashrafshaha397 <ashrafshaha397@gmail.com>
2026-07-07 10:17:05 +0530

    new

--- audio-logic-js/audio-map.js
+++ audio-logic-js/audio-map.js
@@ -6,7 +6,7 @@ const audioMap = {
   // home bg start
 
   home: {
-    bg:"../assets-img-audio/Home-page-img-audio/Arabian-Nights-Instrumental-but-my-favorite-part-is-looped-Arabian-Nights-Desert-Vibes-version.mp3",
+    bg:"assets-img-audio/Home-page-img-audio/Arabian-Nights-Instrumental-but-my-favorite-part-is-looped-Arabian-Nights-Desert-Vibes-version.mp3",
 
     // story:""
   },
